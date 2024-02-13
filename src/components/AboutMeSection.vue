eb626da57f8d8ddce0c06eda9e50962e42c3a14f
Leonardo Solano <lsolano4@gmail.com>
2024-02-13 16:45:03 -0500

    fix: years not appears

--- src/components/AboutMeSection.vue
+++ src/components/AboutMeSection.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container mx-auto text-center">
     <h1 class="font-semibold text-5xl mb-12" data-aos="fade-up" v-html="$t('aboutMe.title')"></h1>
-    <p data-aos="fade-up" v-html="$t('aboutMe.description', { years: new Date().getFullYear() - 2012 })"></p>
+    <p data-aos="fade-up" v-html="$t('aboutMe.description', { years })"></p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const years = ref(new Date().getFullYear() - 2012);
 </script>
-
-<style scoped></style>
